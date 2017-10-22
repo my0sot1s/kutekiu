e85f0585ca7bd9892c52f36451d4a03b75176abe
my0sot1s <manhte231@gmail.com>
2017-10-22 12:38:28 +0700

    slice code

--- client/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-export default require("redux").combineReducers({
-    image: require("./image").default,
-    timeline: require("./timeline").default,
-    profile: require("./profile").default,
-})
