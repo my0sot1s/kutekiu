63ebcc967d5d2970d8f8e865c6e48463ce8e439f
my0sot1s <manhte231@gmail.com>
2017-10-22 22:17:26 +0700

    commit marcarita

--- server/boot/broadcast.js
+++ server/boot/broadcast.js
@@ -1,53 +1,53 @@
 
 
-// /**
-//  * 
-//  * 
-//  * @author te.ng - <manhte231>
-//  * 
-//  * 
-//  * 
-//  */
+/**
+ * 
+ * 
+ * @author te.ng - <manhte231>
+ * 
+ * 
+ * 
+ */
 
-// const netw = require("../../utils/network")
-// const Promise = require("bluebird")
-// const cst = require("../../utils/constants")
+const netw = require("../../utils/network")
+const Promise = require("bluebird")
+const cst = require("../../utils/constants")
 
-// module.exports = function (app) {
-//     /**
-//      * work with queue
-//      * 
-//      * @description người nhận - marcarita
-//      */
-//     netw.listenMessage(cst.PREFIX_SOURCES_QUEUE + "get2Comment", (params) => {
-//         if (params.post_id && Array.isArray(params.post_id)) {
-//             return Promise.map(params.post_id, p => {
-//                 return app.models.social_comments
-//                     .getDetailComment(p)
-//                     .then(log => {
-//                         return { comment: { count: log[0], cmt: log[1] } }
-//                     })
-//                     .catch(err => {
-//                         return err;
-//                     })
-//             })
-//                 .then(comment => {
-//                     return Promise.map(params.post_id, (p, index) => {
-//                         return app.models.social_like
-//                             .getPostLike(p)
-//                             .then(log => {
-//                                 return { like: log, ...comment[index] }
-//                             })
-//                             .catch(err => {
-//                                 return err;
-//                             })
-//                     })
-//                 })
-//                 .then(doc => {
-//                     return netw.sendToQueue(cst.PREFIX_DESTINATIONS_QUEUE + "get2Comment", doc)
-//                 }).catch(err => {
-//                     console.error(err);
-//                 })
-//         } else console.error("Định dạng message? Lỗi")
-//     })
-// }
+module.exports = function (app) {
+    /**
+     * work with queue
+     * 
+     * @description người nhận - marcarita
+     */
+    netw.listenMessage(cst.PREFIX_SOURCES_QUEUE + "get2Comment", (params) => {
+        if (params.post_id && Array.isArray(params.post_id)) {
+            return Promise.map(params.post_id, p => {
+                return app.models.social_comments
+                    .getDetailComment(p)
+                    .then(log => {
+                        return { comment: { count: log[0], cmt: log[1] } }
+                    })
+                    .catch(err => {
+                        return err;
+                    })
+            })
+                .then(comment => {
+                    return Promise.map(params.post_id, (p, index) => {
+                        return app.models.social_like
+                            .getPostLike(p)
+                            .then(log => {
+                                return { like: log, ...comment[index] }
+                            })
+                            .catch(err => {
+                                return err;
+                            })
+                    })
+                })
+                .then(doc => {
+                    return netw.sendToQueue(cst.PREFIX_DESTINATIONS_QUEUE + "get2Comment", doc)
+                }).catch(err => {
+                    console.error(err);
+                })
+        } else console.error("Định dạng message? Lỗi")
+    })
+}
